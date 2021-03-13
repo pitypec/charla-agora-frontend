a9972cf09c1ea72aa9f1c1d7dc1c4f3f4a98f3f6
Adewumi Emmanuel Femi <adewumexemma1@gmail.com>
2021-03-13 15:25:14 +0100

    main commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Header from './component/header/Header';
+import MainPages from './component/mainpages/pages';
+import { DataProvider } from './GlobalState';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DataProvider>
+      <Router>
+        <Header />
+        <MainPages />
+      </Router>
+    </DataProvider>
   );
 }
 
